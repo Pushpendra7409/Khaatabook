d93fc2f9b874ae0b9f89b9e9e5cca487612ce044
Pushpendra7409 <pushpendra7409@gmail.com>
2024-09-14 10:49:20 +0530

    Create new Hisaab page

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
-app.use(express.urlencoded({ extended : true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req,res) => {
-    res.render('index');
+app.get('/', (req, res, next) => {
+    fs.readdir('./hisaab', function (err, files) {
+        if (err) {
+            return next(err); // Use next to pass the error to the error handler
+        }
+        res.render('index', { files: files });
+    });
+});
+
+app.get('/create', (req, res, next) => {
+    res.render('create');
+})
+
+app.post('/createhisaab', (req, res, next) => {
+    fs.writeFile(`./hisaab/${req.body.title}`,req.body.content, function (err){
+        if (err) {
+            return next(err); // Use next to pass the error to the error handler
+        }
+        res.redirect('/');
+    });
+    //res.send(req.body);
 })
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
-})
+});
+
 
